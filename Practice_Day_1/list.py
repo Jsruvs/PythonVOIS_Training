7c0eadf3ddb942b8ca7c28fb0ad7a55e8b5bb0d1
Aman Kumar Gupta <115359377+amangupta65@users.noreply.github.com>
2025-12-24 15:42:31 +0530

    Update list.py

--- Practice_Day_1/list.py
+++ Practice_Day_1/list.py
@@ -1,4 +1,5 @@
-#1
+#1 write a program to remove duplicate elements from a list without using set.
+
 lst = [1, 2, 2, 3, 4, 4, 5]
 unique_list = []
 
@@ -7,25 +8,28 @@ for item in lst:
         unique_list.append(item)
 print(unique_list)
 
-#2
+#2 Given a list of numbers, create a new list containing only even numbers using list comprehension
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 even_numbers = [n for n in numbers if n % 2 == 0]
 print(even_numbers)
 
-#3
+#3 Write a program to find the second largest element in a list
+
 lst = [10, 20, 30, 40, 20]
 unique_lst = list(set(lst))
 unique_lst.sort()
 
 print("Second Largest:", unique_lst[-2])
 
-#4
-nested_list = [[1, 2, 3], [4, 5, 6], [7, 8]]
+#4 Create a nested list and calculate the sum of each inner list
 
-for inner_list in nested_list:
+list = [[1, 2, 3], [4, 5, 6], [7, 8],7]
+for inner_list in list:
     print("Sum:", sum(inner_list))
 
-#5
+#5 Demonstrate shallow copy and deep copy of a list with mutable elements.
+
 import copy
 
 lst1 = [[1, 2], [3, 4]]
